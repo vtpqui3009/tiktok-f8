eacee25ac107dbe1af2b8d2041cfcb4847c58989
vtpqui <vtpqui@gmail.com>
2022-05-09 12:05:04 +0700

    Style Default Layout #1

--- src/components/Layout/DefaultLayout/Sidebar/index.js
+++ src/components/Layout/DefaultLayout/Sidebar/index.js
@@ -1,4 +1,11 @@
+import classNames from "classnames/bind";
+import styles from "./Sidebar.module.scss";
+const cx = classNames.bind(styles);
 const Sidebar = () => {
-  return <h1>Sidebar Page</h1>;
+  return (
+    <aside className={cx("wrapper")}>
+      <h1>Sidebar Page</h1>
+    </aside>
+  );
 };
 export default Sidebar;
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,4 +1,11 @@
+import classNames from "classnames/bind";
+import styles from "./Header.module.scss";
+const cx = classNames.bind(styles);
 const Header = () => {
-  return <h1>Header Page</h1>;
+  return (
+    <header className={cx("wrapper")}>
+      <div className={cx("inner")}></div>
+    </header>
+  );
 };
 export default Header;
